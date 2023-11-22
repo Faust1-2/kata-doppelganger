4a05be194949ed99f138fe9faf84f08004817242
faustin <faustin.dewas@efrei.net>
2023-11-22 10:37:01 +0100

    tests for discount applier

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -2,12 +2,8 @@ from discount_applier import DiscountApplier
 
 
 class MockNotifier:
-
-
     def notify(self, user, message):
         print(f"The user : {user} received {message}")
-
-
 
 def test_apply_v1(capfd):
     mockNotifier = MockNotifier()
@@ -31,6 +27,3 @@ def test_apply_v2(capfd):
     splitingOut = out.split('\n')
     for i in range(len(users)):
         assert splitingOut[i] == (f"The user : {users[i]} received You've got a new discount of {discount}%")
-
-    # TODO: write a test that fails due to the bug in
-    # DiscountApplier.apply_v2
