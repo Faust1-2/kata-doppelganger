da2ae450a0bc2e4e08c736bfbdc92d1f1c2f03d4
faustin <faustin.dewas@efrei.net>
2023-11-22 10:35:52 +0100

    tests for discount_applier

--- python/tests/test_discount_applier.py
+++ python/tests/test_discount_applier.py
@@ -1,10 +1,36 @@
-def test_apply_v1():
-    # TODO: write a test that fails due to the bug in
-    # DiscountApplier.apply_v1
-    pass
+from discount_applier import DiscountApplier
 
 
-def test_apply_v2():
+class MockNotifier:
+
+
+    def notify(self, user, message):
+        print(f"The user : {user} received {message}")
+
+
+
+def test_apply_v1(capfd):
+    mockNotifier = MockNotifier()
+    discount = 25
+    discountApplier = DiscountApplier(mockNotifier)
+    users = [1, 2, 3]
+    discountApplier.apply_v1(discount, users)
+    out, err = capfd.readouterr()
+    splitingOut = out.split('\n')
+    for i in range(len(users)):
+        assert splitingOut[i] == (f"The user : {users[i]} received You've got a new discount of {discount}%")
+
+def test_apply_v2(capfd):
+
+    mockNotifier = MockNotifier()
+    discount = 25
+    discountApplier = DiscountApplier(mockNotifier)
+    users = [1, 2, 3]
+    discountApplier.apply_v2(discount, users)
+    out, err = capfd.readouterr()
+    splitingOut = out.split('\n')
+    for i in range(len(users)):
+        assert splitingOut[i] == (f"The user : {users[i]} received You've got a new discount of {discount}%")
+
     # TODO: write a test that fails due to the bug in
     # DiscountApplier.apply_v2
-    pass
