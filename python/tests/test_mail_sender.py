dedbdda03181fac348d26af2411f725ff14761fb
faustin <faustin.dewas@efrei.net>
2023-11-22 11:20:02 +0100

    Co-authored-by: Elvin Aupiais--Berthy <NovaGamma@users.noreply.github.com> Co-authored-by: FrancoisCharvet <FrancoisCharvet@users.noreply.github.com> Co-authored-by: Moon_001 <Nelson-Fossi@users.noreply.github.com>

--- python/tests/test_mail_sender.py
+++ python/tests/test_mail_sender.py
@@ -1,8 +1,42 @@
-def test_send_v1():
-    # TODO: write a test that fails due to the bug in MailSender.send_v1
-    pass
+from dataclasses import dataclass
+from mail_sender import MailSender, Request
 
+@dataclass
+class Response:
+    request: Request
+    code: int
+
+@dataclass
+class User: 
+    name: str
+    email: str
+
+class MockHttpClient:
+
+    def __init__(self, status) -> None:
+        self.code = status
+
+    def post(self, url, request):
+        return Response(request, self.code)
+
+def test_send_v1():
+    message = 'Hello world'
+    user = User('toto', 'toto@titi.com')
+    mockHttpClient = MockHttpClient(200)
+    mailSender = MailSender(mockHttpClient)
+    request = mailSender.send_v1(user, message).request
+    assert request.name == user.name
+    assert request.email == user.email
+    assert request.subject == 'New notification'
+    assert request.message == message
 
 def test_send_v2():
-    # TODO: write a test that fails due to the bug in MailSender.send_v2
-    pass
+    message = 'Hello world'
+    user = User('toto', 'toto@titi.com')
+    mockHttpClient = MockHttpClient(503)
+    mailSender = MailSender(mockHttpClient)
+    request = mailSender.send_v2(user, message).request
+    assert request.name == user.name
+    assert request.email == user.email
+    assert request.subject == 'New notification'
+    assert request.message == message
